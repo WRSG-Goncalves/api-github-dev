9eeccfec12251c1d0b1470514aa957810f0f3057
WRSG-Goncalves <wilssp@gmail.com>
2023-05-28 14:32:45 -0300

    Configuração de Filtros

---
+++ src/pages/Repositories/Filter/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Filter() {
+  const langs = [
+    {name: 'Javascript', count: 5 , color: '#f1c40f'},
+    {name: 'React', count: 3 , color: '#95a5a6'},
+    {name: 'PHP', count: 2 , color: '#3498db'},
+  ]
+  return <h1>Filter</h1>
+}
+
+export default Filter
